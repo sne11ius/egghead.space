0ba104373ab3e6e16c3af86858796fa18135921e
Cornelius Wichering <cornelius.lilge@gmail.com>
2018-04-19 22:00:29 +0200

    Remove example code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,5 @@
 <template>
   <div>
-    <h1>This is a home page</h1>
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
     <input v-model.trim="newSketchTitle" @keyup.enter="addSketch" placeholder="Add new sketch"><br>
     Here are the sketches:
     <p v-for="sketch of sketches" :key="sketch.id">
@@ -14,7 +11,6 @@
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 import Firebase from "firebase";
 import { db } from "@/firebase";
 
@@ -22,9 +18,7 @@ const sketches = db.collection("sketches");
 
 export default {
   name: "home",
-  components: {
-    HelloWorld
-  },
+  components: {},
   data() {
     return {
       sketches: [],
