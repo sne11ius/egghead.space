f186840ee3b9d9df55d23ec29c5d58cbc20bca8a
Cornelius Wichering <cornelius.lilge@gmail.com>
2018-04-19 20:26:16 +0200

    Added twitter login

--- src/views/SignInDialog.vue
+++ src/views/SignInDialog.vue
@@ -31,14 +31,11 @@ export default {
           }
         },
         signInOptions: [
-          // List of OAuth providers supported.
           firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-          // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-          // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+          firebase.auth.TwitterAuthProvider.PROVIDER_ID,
           firebase.auth.GithubAuthProvider.PROVIDER_ID,
           firebase.auth.EmailAuthProvider.PROVIDER_ID
         ]
-        // Other config options...
       });
     },
     cancel() {
