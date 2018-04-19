917621daa7e95aaf040d794cbfdbeca08189847e
Erik Legenhausen <erik.legenhausen@engram.de>
2018-04-19 17:16:42 +0200

    added email/password authentication

--- src/views/SignInDialog.vue
+++ src/views/SignInDialog.vue
@@ -35,7 +35,8 @@ export default {
           firebase.auth.GoogleAuthProvider.PROVIDER_ID,
           // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
           // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-          firebase.auth.GithubAuthProvider.PROVIDER_ID
+          firebase.auth.GithubAuthProvider.PROVIDER_ID,
+          firebase.auth.EmailAuthProvider.PROVIDER_ID
         ]
         // Other config options...
       });
