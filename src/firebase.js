ef9b9aee545772270207484d3b21c3cb9d9b0197
erle <erik.legenhausen@gmail.com>
2018-04-20 08:14:21 +0200

    linked egghead.space to firebase domain

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,7 @@ import "firebase/firestore";
 // Initialize Firebase
 const config = {
   apiKey: "AIzaSyAtbFdQlWu-oWtN3Y7vpslY0oht5SbCSf8",
-  authDomain: "eggheadspace-e99d7.firebaseapp.com",
+  authDomain: "egghead.space",
   databaseURL: "https://eggheadspace-e99d7.firebaseio.com",
   projectId: "eggheadspace-e99d7",
   storageBucket: "eggheadspace-e99d7.appspot.com",
