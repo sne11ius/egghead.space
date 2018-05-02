7a4340ae43d6addf2ec06df3f4f89cf274811f33
Cornelius Wichering <cornelius.lilge@gmail.com>
2018-05-03 00:14:31 +0200

    Top sketches component with basic controls

---
+++ src/components/TopSketches.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="top-sketches">
+    <v-toolbar tabs>
+      <v-tabs v-model="active">
+        <v-tab ripple>Newest</v-tab>
+        <v-tab ripple>Rating</v-tab>
+        <v-tab ripple>Comments</v-tab>
+        <v-tab-item>
+          <v-card>
+            Newes Sketches here
+          </v-card>
+        </v-tab-item>
+        <v-tab-item>
+          <v-card>
+            Best rated Sketches here
+          </v-card>
+        </v-tab-item>
+        <v-tab-item>
+          <v-card>
+            Most commented Sketches here
+          </v-card>
+        </v-tab-item>
+        <v-spacer></v-spacer>
+        <v-select
+            id="time-period"
+            :items="items"
+            v-model="timePeriod"
+            label="From..."
+            single-line
+            class="tabs__item"
+            solo
+          ></v-select>
+      </v-tabs>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TopSketches",
+  data() {
+    return {
+      timePeriod: "This week",
+      active: "",
+      items: ["This week", "This month", "All time"]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+#time-period {
+  max-width: 153px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,33 @@
 <template>
   <v-container fluid grid-list-lg>
     <v-layout row wrap>
-      <v-flex xs12 md6 lg4 v-for="sketch of orderedSketches" :key="sketch.id">
+      <!--v-flex xs12 md6 lg4 v-for="sketch of orderedSketches" :key="sketch.id">
         <v-card flat tile>
           <SketchSummary :sketch="sketch"></SketchSummary>
         </v-card>
+      </v-flex-->
+      <v-flex xs4>
+        <v-card>
+          Featured
+        </v-card>
+      </v-flex>
+      <v-flex xs4>
+        <v-card>
+          Teaser Text
+        </v-card>
+      </v-flex>
+      <v-flex xs4>
+        <TopSketches></TopSketches>
       </v-flex>
     </v-layout>
-    <v-btn id="create-sketch-button" to="/create" title="Create new sketch" color="primary" fab top right fixed>
+    <!--v-btn id="create-sketch-button" to="/create" title="Create new sketch" color="primary" fab top right fixed>
       <v-icon>add</v-icon>
-    </v-btn>
+    </v-btn-->
   </v-container>
 </template>
 
 <script>
+import TopSketches from "@/components/TopSketches.vue";
 import SketchSummary from "@/components/SketchSummary.vue";
 import "simplemde/dist/simplemde.min.css";
 import { db } from "@/firebase";
@@ -23,7 +37,8 @@ const orderedSketches = db.collection("sketches").orderBy("created", "desc");
 export default {
   name: "Home",
   components: {
-    SketchSummary
+    SketchSummary,
+    TopSketches
   },
   data() {
     return {
