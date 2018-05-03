4afae9cdb04099b0724eb962ecfe736768cc5937
Cornelius Wichering <cornelius.wichering@engram.de>
2018-05-03 07:32:47 +0200

    Removed unwanted box shadow from period selector

--- src/components/TopSketches.vue
+++ src/components/TopSketches.vue
@@ -51,5 +51,7 @@ export default {
 <style lang="scss" scoped>
 #time-period {
   max-width: 153px;
+  box-shadow: 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 2px 0px 0 rgba(0, 0, 0, 0.14),
+    0 1px 0px 0 rgba(0, 0, 0, 0.12);
 }
 </style>
