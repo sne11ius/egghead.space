b1d897c657fcc826eba215d26e2e31dd7c57e57c
Cornelius Wichering <cornelius.lilge@gmail.com>
2018-07-18 23:16:19 +0200

    FirebaseUtil.toDate can now better handle strange data ;)

--- service/FirebaseUtil.js
+++ service/FirebaseUtil.js
@@ -1,6 +1,9 @@
 import { api } from 'api'
 class FirebaseUtil {
   toDate (timestampData) {
+    if (typeof timestampData === 'string') {
+      return new Date(parseInt(timestampData))
+    }
     return new api.firebase.firestore.Timestamp(
       timestampData.seconds,
       timestampData.nanoseconds
